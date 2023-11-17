8d65e10c65f624cc06af44c95668fa9f1b59ea59
서윤 <139292011+uniel12@users.noreply.github.com>
2023-11-17 15:53:09 +0900

    초기 환경설정

---
+++ src/main/java/com/newSummary/NewSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.newSummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSummaryApplication.class, args);
+	}
+
+}
