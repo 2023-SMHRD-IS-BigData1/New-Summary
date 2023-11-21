cc9c2d7c84cc677a82d4f7313a7dc0191bbbb55d
hayoung <iissdn55@gmail.com>
2023-11-21 11:10:11 +0900

    hayoung test

--- src/main/java/com/newSummary/controller/TestController.java
+++ src/main/java/com/newSummary/controller/TestController.java
@@ -2,7 +2,7 @@ package com.newSummary.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//g
 @RestController
 public class TestController {
     @GetMapping("/data")
