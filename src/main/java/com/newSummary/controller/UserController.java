e023d4fc695522eb9de61ec531d4e2585527155b
yun <yunznag00@gmail.com>
2023-12-04 14:53:37 +0900

    로그인한 데이터 확인

--- src/main/java/com/newSummary/controller/UserController.java
+++ src/main/java/com/newSummary/controller/UserController.java
@@ -1,24 +1,22 @@
 package com.newSummary.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.newSummary.auth.PrincipalDetails;
 import com.newSummary.domain.dto.user.JoinRequest;
 import com.newSummary.domain.dto.user.UserDTO;
 import com.newSummary.domain.entity.User;
 import com.newSummary.service.UserService;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -54,52 +52,59 @@ public class UserController {
 
 	// 이메일 중복체크
 	@GetMapping("/users/duplication-email/{userEmail}")
-	public String checkUserEmail(@PathVariable("userEmail") String userEmail) {
+	public ResponseEntity<?> checkUserEmail(@PathVariable("userEmail") String userEmail) {
 		if (userService.checkUserEmailDuplicate(userEmail)) {
 			log.info("userEmail={},message={}",userEmail,"이메일이 중복됩니다.다시 확인해주세요.");
-			return "이메일이 중복됩니다.다시 확인해주세요.";
+			return  ResponseEntity.ok("이메일 중복");
 		} else {
 			log.info("userEmail={},message={}",userEmail,"이메일 사용 가능합니다.");
-			return "이메일 사용 가능합니다.";
+			return  ResponseEntity.ok("이메일 사용가능");
 		}
 
 	}
 
 	// 전화번호 중복확인
 	@GetMapping("/users/duplication-phone/{userPhone}")
-	public String checkNickname(@PathVariable("userPhone") String userPhone) {
+	public ResponseEntity<?> checkNickname(@PathVariable("userPhone") String userPhone) {
 		if (userService.checkUserPhoneDuplicate(userPhone)) {
 			log.info("userPhone={},message={}",userPhone,"전화번호가 중복됩니다.다시 확인해주세요.");
-			return "전화번호 사용 불가합니다.다시 확인해주세요.";
+			return  ResponseEntity.ok("전화번호정보 중복");
 		} else {
 			log.info("userPhone={},message={}",userPhone,"전화번호 사용가능합니다.");
-			return "전화번호 사용 가능합니다.";
+			return ResponseEntity.ok("전화번호 사용가능");
 		}
 	}
 
 
 	// 로그인한 데이터
 	@GetMapping("/users/me")
-	public ResponseEntity<UserDTO> getCurrentUser(Authentication auth) {
-		if (auth == null) {
+	public ResponseEntity<UserDTO> getCurrentUser(@AuthenticationPrincipal PrincipalDetails principalDetails ) {
+		if (principalDetails == null) {
 			return ResponseEntity.badRequest().body(null);
 		}
-		User user = userService.getLoginUserByEmail(auth.getName());
+		User user = userService.getLoginUserByEmail(principalDetails.getUsername());
 		UserDTO userDTO = UserDTO.toUserDTO(user);
 		return ResponseEntity.ok(userDTO);
 	}
 
 	// 회원정보 수정
-	// @PatchMapping("/users/me")
-	// public ResponseEntity<UserDTO> editMypageUser(@RequestBody UserDTO userDTO){
-	// UserDTO originUserDTO = userService.findByUserEmail(userDTO.getUserEmail());
-	// if(userService.checkUserPhoneDuplicate(userDTO.getUserPhone())==false) {
-	// userService.updateUserInfo(originUserDTO)
-	// }
-	//
-	// User user = userService.getLoginUserByEmail(userDTO.getUserEmail());
-	// UserDTO UpdateuserDTO = UserDTO.toUserDTO(user);
-	// return ResponseEntity.ok(UpdateuserDTO);
-	//
-	// }
+//	 @PatchMapping("/users/{userEmail}")
+//	 public ResponseEntity<UserDTO> editMypageUser(@PathVariable("userEmail") String userEmail,
+//			 									   @RequestBody UserDTO userDTO){
+//	 UserDTO originUserDTO = userService.findByUserEmail(userDTO.getUserEmail());
+//	 if(userService.checkUserPhoneDuplicate(userDTO.getUserPhone())==false) {
+//	 userService.updateUserInfo(originUserDTO);
+//	 }
+//	
+//	 User user = userService.getLoginUserByEmail(userDTO.getUserEmail());
+//	 UserDTO UpdateuserDTO = UserDTO.toUserDTO(user);
+//	 return ResponseEntity.ok(UpdateuserDTO);
+//	
+//	 }
+//	 // 회원 정보 삭제
+//	 @DeleteMapping("/users/{userEmail}")
+//	 public String deleteUser (@PathVariable("userEmail") String userEmail) {
+//		 
+//	 }
+	
 }
