ed3f259fd44042c81be630a65cd3f75f4ee56e80
김한주 <139292656+HANJU2725@users.noreply.github.com>
2023-12-04 16:17:06 +0900

    키워드 및 뉴스 데이터 최신화

---
+++ src/main/java/com/newSummary/controller/KeywordController.java
@@ -0,0 +1,32 @@
+package com.newSummary.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newSummary.domain.entity.Keyword;
+import com.newSummary.service.KeywordService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/keyword")
+public class KeywordController {
+	
+	@Autowired
+	private final KeywordService keywordService;
+	
+	@GetMapping("/list")
+	public List<Keyword> getlist() {
+		return keywordService.getlist();
+	}
+	@GetMapping("/today")
+	public Keyword getKeyword() {
+		return keywordService.getkeyword();
+	}
+
+}
--- src/main/java/com/newSummary/repository/KeywordsRepository.java
+++ src/main/java/com/newSummary/repository/KeywordsRepository.java
@@ -1,10 +1,12 @@
 package com.newSummary.repository;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.newSummary.domain.entity.Keyword;
 
 public interface KeywordsRepository extends MongoRepository<Keyword, String> {
 	
-	// 
+    // 특정 날짜의 데이터 조회
+    Keyword findByKeywordWriteTime(String date);
 }
