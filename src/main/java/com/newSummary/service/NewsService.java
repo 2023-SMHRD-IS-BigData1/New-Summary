c2b0121e3bba36b93e38dd19266ae5201cd54062
김한주 <139292656+HANJU2725@users.noreply.github.com>
2023-12-06 09:59:46 +0900

    오늘자키워드뉴스

--- src/main/java/com/newSummary/service/NewsService.java
+++ src/main/java/com/newSummary/service/NewsService.java
@@ -1,5 +1,7 @@
 package com.newSummary.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -29,112 +31,130 @@ public class NewsService {
 	private final NewsRepository newsRepository;
 	private final KeywordService keywordService;
 
-	// 전체 리스트 조회(기사 작성시간 내림차순)
-	public List<NewsDTO> getNewsList() {
-		List<News> newsList = this.newsRepository.findAllByOrderByArticleWriteTimeDesc();
-		System.out.println("돼냐? " + newsList.size());
+	   // 전체 리스트 조회(기사 작성시간 내림차순)
+	   public List<NewsDTO> getNewsList() {
+	      List<News> newsList = this.newsRepository.findAllByOrderByArticleWriteTimeDesc();
+	      System.out.println("돼냐? " + newsList.size());
 
-		// DTO 변환
-		List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
+	      // DTO 변환
+	      List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
 
-		return newsDTOList;
-	}
-	// 상세 리스트 조회 + 조회수 증가
-	public NewsDTO detailNews(String id) {
-		Optional<News> on = this.newsRepository.findById(id);
-		if (on.isPresent()) {
-			News news = on.get();
-			news.incrementViewCount();
-			this.newsRepository.save(news);
-			NewsDTO newsDTO = convertToDTO(news);
-			return newsDTO;
-		} else {
-			return null;
-		}
-	}
-	// 엔터티를 DTO로 변환하는 메소드
-	private NewsDTO convertToDTO(News news) {
-		NewsDTO newsDTO = new NewsDTO();
-		newsDTO.setId(news.getId());
-		newsDTO.setTitle(news.getTitle());
-		newsDTO.setReporter(news.getReporter());
-		newsDTO.setArticleWriteTime(news.getArticleWriteTime());
-		newsDTO.setPicture(news.getPicture());
-		newsDTO.setArticleContent(news.getArticleContent());
-		newsDTO.setPress(news.getPress());
-		newsDTO.setUrl(news.getUrl());
-		newsDTO.setCategory(news.getCategory());
-		newsDTO.setViewCount(news.getViewCount());
-		newsDTO.setSummary(news.getSummary());
-		return newsDTO;
-	}
-	// 검색 서비스
-	public List<NewsDTO> searchNews(String term) {
-		List<News> newsList = newsRepository.findByTitleRegexOrReporterRegexOrArticleContentRegexIgnoreCase(term, term,
-				term);
-		List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
-		return newsDTOList;
-	}
-	// 카테고리 뉴스 데이터
-	public List<NewsDTO> cateNews(String category) {
-		List<News> newsList = newsRepository.findByCategory(category);
-		List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
-		return newsDTOList;
-	}
-	// 카테고리 랜덤 뉴스 데이터
-	public List<NewsDTO> cateRandomNews(String category) {
-		List<News> newsList = newsRepository.findByCategory(category);
-		List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
-		List<NewsDTO> randomNewsList = getRandomNews(newsDTOList, 5);
-		return randomNewsList;
-	}
-    // 랜덤 뉴스 리스트에 담는 메소드
-    private List<NewsDTO> getRandomNews(List<NewsDTO> newsList, int count) {
-        List<NewsDTO> randomNewsList = new ArrayList<>();
-        Random random = new Random();
+	      return newsDTOList;
+	   }
+	   // 상세 리스트 조회 + 조회수 증가
+	   public NewsDTO detailNews(String id) {
+	      Optional<News> on = this.newsRepository.findById(id);
+	      if (on.isPresent()) {
+	         News news = on.get();
+	         news.incrementViewCount();
+	         this.newsRepository.save(news);
+	         NewsDTO newsDTO = convertToDTO(news);
+	         return newsDTO;
+	      } else {
+	         return null;
+	      }
+	   }
+	   // 엔터티를 DTO로 변환하는 메소드
+	   private NewsDTO convertToDTO(News news) {
+	      NewsDTO newsDTO = new NewsDTO();
+	      newsDTO.setId(news.getId());
+	      newsDTO.setTitle(news.getTitle());
+	      newsDTO.setReporter(news.getReporter());
+	      newsDTO.setArticleWriteTime(news.getArticleWriteTime());
+	      newsDTO.setPicture(news.getPicture());
+	      newsDTO.setArticleContent(news.getArticleContent());
+	      newsDTO.setPress(news.getPress());
+	      newsDTO.setUrl(news.getUrl());
+	      newsDTO.setCategory(news.getCategory());
+	      newsDTO.setViewCount(news.getViewCount());
+	      newsDTO.setSummary(news.getSummary());
+	      return newsDTO;
+	   }
+	   // 검색 서비스
+	   public List<NewsDTO> searchNews(String term) {
+	      List<News> newsList = newsRepository.findByTitleRegexOrReporterRegexOrArticleContentRegexIgnoreCase(term, term,
+	            term);
+	      List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
+	      return newsDTOList;
+	   }
+	   // 카테고리 뉴스 데이터
+	   public List<NewsDTO> cateNews(String category) {
+	      List<News> newsList = newsRepository.findByCategory(category);
+	      List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
+	      return newsDTOList;
+	   }
+	   // 카테고리 랜덤 뉴스 데이터
+	   public List<NewsDTO> cateRandomNews(String category) {
+	      List<News> newsList = newsRepository.findByCategory(category);
+	      List<NewsDTO> newsDTOList = newsList.stream().map(this::convertToDTO).collect(Collectors.toList());
+	      List<NewsDTO> randomNewsList = getRandomNews(newsDTOList, 5);
+	      return randomNewsList;
+	   }
+	    // 랜덤 뉴스 리스트에 담는 메소드
+	    private List<NewsDTO> getRandomNews(List<NewsDTO> newsList, int count) {
+	        List<NewsDTO> randomNewsList = new ArrayList<>();
+	        Random random = new Random();
 
-        Set<Integer> selectedIndices = new HashSet<>();
-        int newsListSize = newsList.size();
+	        Set<Integer> selectedIndices = new HashSet<>();
+	        int newsListSize = newsList.size();
 
-        while (randomNewsList.size() < count && selectedIndices.size() < newsListSize) {
-            int randomIndex = random.nextInt(newsListSize);
-            if (selectedIndices.add(randomIndex)) {
-                randomNewsList.add(newsList.get(randomIndex));
-            }
-        }
-        return randomNewsList;
-    }
-	// 키워드 뉴스 데이터
-	public List<NewsDTO> keywordNews(){
-		Keyword keyword = keywordService.getkeyword();
-		Map<String, Integer> keywordMap = keyword.getKeywordsData();
-		List<String> topKeys = findTopKeys(keywordMap, 4);
-		List<NewsDTO> newsDTOList = new ArrayList<NewsDTO>();
-	    for(String key : topKeys) {
-	        List<News> newsList = newsRepository.findByTitleRegexOrReporterRegexOrArticleContentRegexIgnoreCase(key, key, key);
-
-	        for(News news : newsList) {
-	            NewsDTO dto = convertToDTO(news);
-	            dto.setKeyword(key);
-	            newsDTOList.add(dto);
+	        while (randomNewsList.size() < count && selectedIndices.size() < newsListSize) {
+	            int randomIndex = random.nextInt(newsListSize);
+	            if (selectedIndices.add(randomIndex)) {
+	                randomNewsList.add(newsList.get(randomIndex));
+	            }
 	        }
+	        return randomNewsList;
 	    }
-	    return newsDTOList;
-	}
-	// 상위키워드 추출
-    public static List<String> findTopKeys(Map<String, Integer> map, int topCount) {
-        // Map.Entry를 리스트로 변환
-        List<Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+	   // 오늘자 키워드 뉴스 데이터
+	   public List<NewsDTO> keywordNews(){
+	      Keyword keyword = keywordService.getkeyword();
+	      Map<String, Integer> keywordMap = keyword.getKeywordsData();
+	      List<String> topKeys = findTopKeys(keywordMap, 4);
+	      List<NewsDTO> newsDTOList = new ArrayList<NewsDTO>();
+	       for(String key : topKeys) {
+	           List<News> newsList = newsRepository.findByTitleRegexOrReporterRegexOrArticleContentRegexIgnoreCase(key, key, key);
+	           System.out.println(key);
+	           newsList = filterTodayNews(newsList);
+	           for(News news : newsList) {
+	               NewsDTO dto = convertToDTO(news);
+	               dto.setKeyword(key);
+	               newsDTOList.add(dto);
+	           }
+	       }
+	       return newsDTOList;
+	   }
+	   // 상위키워드 추출
+	    public static List<String> findTopKeys(Map<String, Integer> map, int topCount) {
+	        // Map.Entry를 리스트로 변환
+	        List<Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
 
-        // 값으로 내림차순 정렬
-        entryList.sort(Entry.comparingByValue(Comparator.reverseOrder()));
+	        // 값으로 내림차순 정렬
+	        entryList.sort(Entry.comparingByValue(Comparator.reverseOrder()));
 
-        // 상위 키 추출
-        List<String> topKeys = new ArrayList<>();
-        for (int i = 0; i < Math.min(topCount, entryList.size()); i++) {
-            topKeys.add(entryList.get(i).getKey());
-        }
+	        // 상위 키 추출
+	        List<String> topKeys = new ArrayList<>();
+	        for (int i = 0; i < Math.min(topCount, entryList.size()); i++) {
+	            topKeys.add(entryList.get(i).getKey());
+	        }
 
-        return topKeys;
-    }
+	        return topKeys;
+	    }
+	    // 오늘 뉴스만 필터링하는 메소드
+	    private List<News> filterTodayNews(List<News> newsList) {
+	        // 현재 날짜 가져오기
+	        String currentDateStr = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy. MM. d"));
+
+	        // 뉴스 리스트를 필터링
+	        return newsList.stream()
+	                .filter(news -> {
+	                    // news.getArticleWriteTime()의 길이가 충분한 경우에만 처리
+	                    if (news.getArticleWriteTime().length() >= currentDateStr.length()) {
+	                        String articleDateStr = news.getArticleWriteTime().substring(0, currentDateStr.length());
+	                        return articleDateStr.equals(currentDateStr);
+	                    }
+	                    return false;
+	                })
+	                .collect(Collectors.toList());
+	    }
 }
