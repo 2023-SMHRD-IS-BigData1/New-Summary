6cd3ad8958de0b6c46b7da934b24647dbd14606f
김하영 <iissdn55@gmail.com>
2023-12-11 10:44:23 +0900

    유저닉네임중복체크, 이메일 유효성 검사

--- src/main/java/com/newSummary/controller/UserController.java
+++ src/main/java/com/newSummary/controller/UserController.java
@@ -2,6 +2,7 @@ package com.newSummary.controller;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -39,11 +40,17 @@ public class UserController {
 	public ResponseEntity<?> join(@Valid @RequestBody JoinRequest joinRequest, BindingResult bindingResult) {
 		// 이메일 중복 체크
 		if (userService.checkUserEmailDuplicate(joinRequest.getUserEmail())) {
-			bindingResult.addError(new FieldError("joinRequest", " userEmail", "이메일이 중복됩니다."));
+			bindingResult.addError(new FieldError("joinRequest", "userEmail", "이메일이 중복됩니다."));
 		}
+
+		// 닉네임 중복 체크
+		if (userService.checkUserNameDuplicate(joinRequest.getUserName())) {
+			bindingResult.addError(new FieldError("joinRequest", "userName", "닉네임이 중복됩니다."));
+		}
+
 		// 전화번호 중복 체크
-		if (userService.checkUserEmailDuplicate(joinRequest.getUserPhone())) {
-			bindingResult.addError(new FieldError("joinRequest", " userPhone", "전화번호가 중복됩니다."));
+		if (userService.checkUserPhoneDuplicate(joinRequest.getUserPhone())) {
+			bindingResult.addError(new FieldError("joinRequest", "userPhone", "전화번호가 중복됩니다."));
 		}
 
 		// password와 passwordCheck가 같은지 체크
@@ -61,15 +68,28 @@ public class UserController {
 
 	// 이메일 중복체크
 	@GetMapping("/users/duplication-email/{userEmail}")
-	public Boolean checkUserEmail(@PathVariable("userEmail") String userEmail) {
-		if (userService.checkUserEmailDuplicate(userEmail) == true) {
-			log.info("userEmail={},message={}", userEmail, "이메일이 중복됩니다.다시 확인해주세요.");
-			return true;
-		} else {
-			log.info("userEmail={},message={}", userEmail, "이메일 사용 가능합니다.");
-			return false;
+	public ResponseEntity<String> checkUserEmail(@PathVariable("userEmail") String userEmail) {
+		// 이메일 형식 검사
+		if (!isValidEmail(userEmail)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("유효한 이메일 형식이 아닙니다.");
 		}
 
+		// 이메일 중복 체크
+		if (userService.checkUserEmailDuplicate(userEmail)) {
+			log.info("userEmail={}, message={}", userEmail, "이메일이 중복됩니다. 다시 확인해주세요.");
+			return ResponseEntity.ok("이메일이 중복됩니다. 다시 확인해주세요.");
+		} else {
+			log.info("userEmail={}, message={}", userEmail, "이메일 사용 가능합니다.");
+			return ResponseEntity.ok("이메일 사용 가능합니다.");
+		}
+	}
+
+	// 이메일 형식 검사 메서드
+	private boolean isValidEmail(String email) {
+		// 여기에 이메일 형식을 검사하는 로직 추가
+		// 예: 정규 표현식 사용
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+		return email.matches(emailRegex);
 	}
 
 	// 전화번호 중복확인
@@ -106,10 +126,9 @@ public class UserController {
 
 	// 회원정보 수정
 	@PatchMapping("/users/{userEmail}")
-	public UserDTO editMypageUser(@PathVariable("userEmail") String userEmail,
-								  @RequestBody UserDTO userDTO) {
+	public UserDTO editMypageUser(@PathVariable("userEmail") String userEmail, @RequestBody UserDTO userDTO) {
 		if (userService.checkUserPhoneDuplicate(userDTO.getUserPhone()) == false) {
-			log.info("userUpdate : userEmail={}, message={} ", userEmail,"회원정보 update 성공");
+			log.info("userUpdate : userEmail={}, message={} ", userEmail, "회원정보 update 성공");
 			UserDTO originUserDTO = userService.findByUserEmail(userEmail);
 			userService.updateUserInfo(originUserDTO, userDTO);
 
@@ -117,7 +136,7 @@ public class UserController {
 			UserDTO UpdateuserDTO = UserDTO.toUserDTO(user);
 			return UpdateuserDTO;
 		} else {
-			log.info("userUpdate : userEmail={}, message={} ", userEmail,"회원정보 update 실패");
+			log.info("userUpdate : userEmail={}, message={} ", userEmail, "회원정보 update 실패");
 			return null;
 		}
 
@@ -126,28 +145,31 @@ public class UserController {
 	// 회원 정보 삭제
 	@DeleteMapping("/users/{userEmail}")
 	public void deleteUser(@PathVariable("userEmail") String userEmail) {
-		log.info("delete : userEmail={}, message={}", userEmail,"삭제 성공");
+		log.info("delete : userEmail={}, message={}", userEmail, "삭제 성공");
 		userService.userDelete(userEmail);
 	}
 
 	// 회원 프로필 업로드
 	@PostMapping("/user/profile/{userEmail}")
-	public ResponseEntity<?> uploadProfilePhoto(@PathVariable("userEmail") String userEmail, @RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
-		//S3 Bucket 내부에 "/profile"
+	public ResponseEntity<?> uploadProfilePhoto(@PathVariable("userEmail") String userEmail,
+			@RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
+		// S3 Bucket 내부에 "/profile"
 
 		FileUploadResponse profile = s3UploaderService.upload(userEmail, multipartFile, "profile");
 		return ResponseEntity.ok(profile);
 	}
+
 	// 회원 프로필 수정
-    @PutMapping("/user/profile/{userEmail}")
-    public ResponseEntity<?> updateProfilePhoto(@PathVariable("userEmail") String userEmail, @RequestParam("newProfilePhoto") MultipartFile newProfilePhoto) throws IOException {
-        // S3 Bucket 내부에 "/profile"
-        String dirName = "profile";
+	@PutMapping("/user/profile/{userEmail}")
+	public ResponseEntity<?> updateProfilePhoto(@PathVariable("userEmail") String userEmail,
+			@RequestParam("newProfilePhoto") MultipartFile newProfilePhoto) throws IOException {
+		// S3 Bucket 내부에 "/profile"
+		String dirName = "profile";
 
-        // 프로필 수정 메소드 호출
-        FileUploadResponse updatedProfile = s3UploaderService.updateProfile(userEmail, newProfilePhoto, dirName);
+		// 프로필 수정 메소드 호출
+		FileUploadResponse updatedProfile = s3UploaderService.updateProfile(userEmail, newProfilePhoto, dirName);
 
-        return ResponseEntity.ok(updatedProfile);
-    }
+		return ResponseEntity.ok(updatedProfile);
+	}
 
 }
